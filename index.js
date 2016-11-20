3a2cc7407c883ee39d2b2b7df57c4341c835e8f3
bokuweb <bokuweb12@gmail.com>
2016-11-21 02:15:01 +0900

    Fix browser and load script abs path

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var path = require('path');
+var glob = require("glob")
+
 var NightmareBrowser = function (baseBrowserDecorator, args, config) {
-  console.log(config);
-  console.log(this)
+  const files = glob.sync('./**/node_modules/karma-nightmare/lib/browser.js');
   baseBrowserDecorator(this)
   this._start = function (url) {
-    this._execCommand('node', [path.resolve('lib/browser.js'), url])
+    this._execCommand('node', [path.resolve(files[0]), url])
   }
 }
 
