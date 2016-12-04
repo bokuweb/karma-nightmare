c1deaa87dc3feb14d5cd870a40c08041dbef007a
bokuweb <bokuweb12@gmail.com>
2016-12-04 15:26:02 +0900

    update

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
 declare module "karma-nightmare" {
   export function screenshot(path: string): Promise<void>;
-  export function isNightmare(void): boolean;
+  export function isNightmare(): boolean;
 }
-
