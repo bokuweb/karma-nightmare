debcf56f84965fd0756293931770f72004a46a70
bokuweb <bokuweb12@gmail.com>
2016-12-25 15:41:08 +0900

    re add browser.js

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
 var path = require('path');
 var client = require("./client");
-var file = path.join(__dirname, 'lib/custom-script.js');
 
 var NightmareBrowser = function (baseBrowserDecorator, args, config) {
+  const file = path.resolve(path.dirname(require.resolve('karma-nightmare')), 'lib/browser.js');
   baseBrowserDecorator(this);
-  const webPreferences = Object.assign(
-    config.nightmareOptions.webPreferences || {},
-    {
-      preload: file,
-      nodeIntegration: true,
-    }
-  );
-
   this._start = function (url) {
-    const nightmare = require('nightmare')(
-      Object.assign({
-        width: 800,
-        height: 600,
-      }, config.nightmareOptions || {}, {
-        waitTimeout: 1000000000,
-        webPreferences
-      })
-    );
-
-    nightmare
-      .goto(url)
-      .wait('#browsers')
-      .evaluate(() => {
-        document.querySelector('#banner').style.display = 'none';
-        document.querySelector('#browsers').style.display = 'none';
-      })
-      .wait(() => null)
-      .end()
-      .catch((error) => console.error(error));
+    this._execCommand('node', [
+      file,
+      url,
+      JSON.stringify(config.nightmareOptions),
+    ])
   }
 }
 
