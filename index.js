61f908786644eb5fa71e5c6e0ba512351a0b9f95
laco0416 <laco0416+github@gmail.com>
2017-02-13 17:07:29 +0900

    Allow karma config without nightmareOptions

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var path = require('path');
 var client = require("./client");
 
 var NightmareBrowser = function (baseBrowserDecorator, args, config) {
+  const options = config.nightmareOptions || {};
   const file = path.join(__dirname, 'lib/browser.js');
   baseBrowserDecorator(this);
   this._start = function (url) {
     this._execCommand('node', [
       file,
       url,
-      JSON.stringify(config.nightmareOptions),
+      JSON.stringify(options),
     ]);
 
     this._process.stderr.on('data', function (data) {
